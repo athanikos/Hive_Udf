379c8be37e4a2143881135a42231189387415b02
athanikos <athanikos@gmail.com>
2019-07-15 23:02:03 +0300

    Update TestLineParser.java

--- udfs/src/test/java/crmlbd/udfs/TestLineParser.java
+++ udfs/src/test/java/crmlbd/udfs/TestLineParser.java
@@ -1,11 +1,8 @@
 package crmlbd.udfs;
-
 import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
-
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.codehaus.commons.compiler.CompileException;
 import org.junit.Test;
@@ -15,7 +12,7 @@ public class TestLineParser {
 	public void TEstStringToString() throws CompileException, InvocationTargetException, HiveException, IOException
 	{
 		HashMap<String, String> lookup= new HashMap<String, String>();
-	    lookup.put("ActiveCards" ,"x>=0 && x<=3 ? \"01\" : \"00\""   );
+		lookup.put("ActiveCards" ,"x>=0 && x<=3 ? \"01\" : \"00\""   );
 		assertEquals(LineParser.getRightPart("ActiveCards", lookup),"x>=0 && x<=3 ? \"01\" : \"00\"");
     } 
 }
