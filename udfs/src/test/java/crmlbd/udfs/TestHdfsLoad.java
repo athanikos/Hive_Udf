247739aaf9abcc62a64fa3736d18108cd6f20422
athanikos <athanikos@gmail.com>
2019-07-15 23:02:30 +0300

    Update TestHdfsLoad.java

--- udfs/src/test/java/crmlbd/udfs/TestHdfsLoad.java
+++ udfs/src/test/java/crmlbd/udfs/TestHdfsLoad.java
@@ -1,5 +1,4 @@
 package crmlbd.udfs;
-
 import static org.junit.Assert.*;
 import java.util.regex.Pattern;
 import org.junit.Test;
@@ -18,7 +17,7 @@ public class TestHdfsLoad {
     {
     	String line = "AgeBand:x==01,x==02,x==03|16-25,26-40,41-60";
    		String[] items = line.split(Pattern.quote(":"));
-    		assertEquals(items[0],"AgeBand");
+        assertEquals(items[0],"AgeBand");
     }
    
 }
